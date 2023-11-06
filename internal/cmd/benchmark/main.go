88d1231c4d489bbdbacbfe0b1d2251bee2689ce7
Volume999 <Volume99917@gmail.com>
2023-11-06 19:23:35 +0100

    Add benchmarking generator

--- internal/cmd/benchmark/main.go
+++ internal/cmd/benchmark/main.go
@@ -1,6 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"POCS_Projects/internal/cmd/benchmark/dataloaders"
+	commands2 "POCS_Projects/internal/config"
+	"fmt"
+	"time"
+)
+
+func benchmarkDataGeneration() {
+	// Try generating data
+	constants := commands2.NewConstants()
+	times := 0
+	for i := 0; i < 10; i++ {
+		start := time.Now()
+		_, _ = dataloaders.NewDataGeneratorImpl(10, constants, nil).GenerateData()
+		t := time.Since(start).Seconds()
+		fmt.Println(t)
+		times += int(t)
+	}
+	fmt.Println("Average time taken: ", times/10.0)
+}
 
 func main() {
 	fmt.Println("Hello, benchmark")
