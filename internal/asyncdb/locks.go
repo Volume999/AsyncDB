8f8942da563f1bd8012bdb7f2d5e591e71ea2f16
Volume999 <Volume99917@gmail.com>
2024-04-12 14:04:41 +0200

    Fix [AsyncDB]: Add LockManager Lock function

--- internal/asyncdb/locks.go
+++ internal/asyncdb/locks.go
@@ -2,11 +2,20 @@ package asyncdb
 
 import "github.com/google/uuid"
 
+type TransactionStatusChecker interface {
+	TransactionStatusCheck(tid uuid.UUID) bool
+}
+
 type LockManager interface {
-	ReleaseLocks(id uuid.UUID) error
+	Lock(tableId interface{}, key interface{}) error
+	ReleaseLocks(tid uuid.UUID) error
 }
 
 type LockManagerImpl struct {
+}
+
+func (m LockManagerImpl) Lock(tableId interface{}, key interface{}) error {
+	return nil
 }
 
 func (m LockManagerImpl) ReleaseLocks(id uuid.UUID) error {
