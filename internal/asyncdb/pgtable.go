acf89110603ff4d1459c6efb8c3da7e9bc90020a
Volume999 <Volume99917@gmail.com>
2024-04-24 10:45:59 +0200

    WIP [AsyncDB]: Use pool instead of single connection

--- internal/asyncdb/pgtable.go
+++ internal/asyncdb/pgtable.go
@@ -3,34 +3,34 @@ package asyncdb
 import (
 	"context"
 	"fmt"
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"os"
 	"time"
 )
 
 type PgTableFactory struct {
-	conn *pgx.Conn
+	pool *pgxpool.Pool
 }
 
 func NewPgTableFactory(connectionString string) *PgTableFactory {
-	conn, err := pgx.Connect(context.Background(), connectionString)
+	conn, err := pgxpool.New(context.Background(), connectionString)
 	if err != nil {
 		fmt.Errorf("failed to connect to database: %w", err)
 		os.Exit(1)
 	}
-	return &PgTableFactory{conn: conn}
+	return &PgTableFactory{pool: conn}
 }
 
 func (f *PgTableFactory) Close() {
-	if f.conn != nil {
-		f.conn.Close(context.Background())
+	if f.pool != nil {
+		f.pool.Close()
 	}
 }
 
 func (f *PgTableFactory) CreateTable(name string) (Table, error) {
 	ctx, _ := context.WithTimeout(context.Background(), 120*time.Second)
 	query := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (key INT PRIMARY KEY, value VARCHAR(500))", name)
-	_, err := f.conn.Exec(ctx, query)
+	_, err := f.pool.Exec(ctx, query)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create table: %w", err)
 	}
