d20e9ba3836f03c4f64a50cc05a2900c81b2d4fe
Volume999 <Volume99917@gmail.com>
2024-04-24 10:13:10 +0200

    Feat [AsyncDB]: Add skeleton for pgTable

--- internal/asyncdb/pgtable.go
+++ internal/asyncdb/pgtable.go
@@ -1,1 +1,44 @@
 package asyncdb
+
+type PgTableFactory struct {
+}
+
+func NewPgTableFactory() *PgTableFactory {
+	return &PgTableFactory{}
+}
+
+func (f *PgTableFactory) CreateTable(name string) (Table, error) {
+	return NewPgTable(name)
+}
+
+type PgTable struct {
+}
+
+func (p PgTable) Name() string {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (p PgTable) Get(key interface{}) (value interface{}, err error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (p PgTable) Put(key interface{}, value interface{}) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (p PgTable) Delete(key interface{}) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (p PgTable) ValidateTypes(key interface{}, value interface{}) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func NewPgTable(name string) (*PgTable, error) {
+	return &PgTable{}, nil
+}
